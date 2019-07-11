21e656547f7bb6e5d9c22b285d8fa7be9fc7ee36
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-07-11 22:11:28 +0100

    Solve kata

---
+++ spec/bingo/bingo.rb
@@ -0,0 +1,9 @@
+def bingo(tickets, n)
+  wins = tickets.map do |ticket|
+    chars, winning_num = ticket
+    chars.split('').any? { |char| char.ord === winning_num } ? 1 : 0
+  end
+  .sum
+
+  wins >= n ? 'Winner!' : 'Loser!'
+end
