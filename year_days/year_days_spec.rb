c06057e42f6285df3f8004ff8a5c3db856e5a64d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-08-16 11:37:47 +0100

    Fix broken tests

--- year_days/year_days_spec.rb
+++ year_days/year_days_spec.rb
@@ -2,17 +2,17 @@ require_relative 'year_days'
 
 describe :year_days do
   it 'returns 366 days for leap years' do
-    expect(year_days?(0)).to be 366
-    expect(year_days(64)).to eq 366
-    expect(year_days(2000)).to eq 366
-    expect(year_days(2016)).to eq 366
+    expect(year_days(0)).to eq '0 has 366 days'
+    expect(year_days(64)).to eq '64 has 366 days'
+    expect(year_days(2000)).to eq '2000 has 366 days'
+    expect(year_days(2016)).to eq '2016 has 366 days'
   end
 
   it 'returns 365 for non-leap years' do
-    expect(year_days(666)).to eq 365
-    expect(year_days(1857)).to eq 365
-    expect(year_days(1974)).to eq 365
-    expect(year_days(300)).to eq 365
+    expect(year_days(666)).to eq '666 has 365 days'
+    expect(year_days(1857)).to eq '1857 has 365 days'
+    expect(year_days(1974)).to eq '1974 has 365 days'
+    expect(year_days(300)).to eq '300 has 365 days'
   end
 end
 
