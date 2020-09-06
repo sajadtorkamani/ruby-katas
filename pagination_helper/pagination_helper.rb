9bfe03a257809e3b1dad33860d53303889fd9e8b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-09-06 20:15:31 +0100

    Solve kata

---
+++ pagination_helper/pagination_helper.rb
@@ -0,0 +1,29 @@
+class PaginationHelper
+  attr_reader :collection, :page_size
+
+  def initialize(collection, page_size)
+    @collection = collection
+    @page_size = page_size
+  end
+
+  def page_count
+    item_count.fdiv(page_size).ceil
+  end
+
+  def item_count
+    collection.count
+  end
+
+  def page_item_count(page_index)
+    start_index = page_index * page_size
+    end_index = (page_index + 1) * page_size
+
+    return -1 if start_index >= item_count
+
+    collection[start_index...end_index].count
+  end
+
+  def page_index(item_index)
+    item_index >= item_count || item_index.negative? ? -1 : item_index / page_size
+  end
+end
