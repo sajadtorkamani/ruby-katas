f2a9b89d21d705d2c674f1c936a141f3073ac244
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-09-10 07:37:27 +0100

    Solve kata

---
+++ process_2arrays/process_2arrays.rb
@@ -0,0 +1,11 @@
+def process_2arrays(arr1, arr2)
+  set1 = arr1.to_set
+  set2 = arr2.to_set
+
+  common = (set1 & set2).count
+  uncommon_items = (set1 + set2) - common
+  d1 = set1 - set2
+  set2_difference = set2 - set1
+
+  [common, uncommon_items, d1, set2_difference].map(&:count)
+end
