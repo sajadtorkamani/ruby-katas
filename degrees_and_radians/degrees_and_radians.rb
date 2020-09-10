02d16040451a4899d5ec027eea9fdd562c6a141b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-09-10 11:28:11 +0100

    Solve kata

---
+++ degrees_and_radians/degrees_and_radians.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/FormatString
+# rubocop:disable Style/FormatStringToken
+module Math
+  def self.degrees(radian)
+    '%gdeg' % (radian * (180 / Math::PI)).round(2)
+  end
+
+  def self.radians(degrees)
+    '%grad' % (degrees * (Math::PI / 180)).round(2)
+  end
+end
+# rubocop:enable Style/FormatString
+# rubocop:enable Style/FormatStringToken
