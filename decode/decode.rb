3d9352f73054380e3ca29e0de988798bd61a4d0e
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-09-27 09:42:59 +0100

    Solve kata

---
+++ decode/decode.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+ALPHABET = ('a'..'z').to_a
+
+def decode(str)
+  str.each_char.map { |c| decode_char(c) }.join('')
+end
+
+def decode_char(char)
+  return ' ' if char == ' '
+
+  char_index = ALPHABET.index(char)
+  decoded_char_index = 25 - char_index
+
+  ALPHABET[decoded_char_index]
+end
