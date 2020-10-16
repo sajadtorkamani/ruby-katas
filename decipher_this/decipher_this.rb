0bef5d1233ec4e5aeeb7b6960f764f8a710493b5
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-10-16 09:58:25 +0100

    Solve kata

---
+++ decipher_this/decipher_this.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+def decipher_this(str)
+  str.split(' ').map { |word| decipher_word(word) }.join(' ')
+end
+
+def decipher_word(word)
+  word = word.gsub(/^\d+/) { |char_code| char_code.to_i.chr }
+
+  return word if word.length == 1
+
+  second_char = word[1]
+  last_char = word[-1]
+
+  word[1] = last_char
+  word[-1] = second_char
+
+  word
+end
