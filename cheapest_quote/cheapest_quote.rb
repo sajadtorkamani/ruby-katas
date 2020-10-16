91e4df5ec1783359918407b64560c7fc4ffb9142
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-10-16 10:33:50 +0100

    Solve kata

---
+++ cheapest_quote/cheapest_quote.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Not the cleanest!
+# rubocop:disable Metrics/AbcSize
+def cheapest_quote(num_newspapers)
+  num_forties, remainder = calculate_num_bundles(num_newspapers, 40)
+  num_twenties, remainder = calculate_num_bundles(remainder, 20)
+  num_tens, remainder = calculate_num_bundles(remainder, 10)
+  num_fives, remainder = calculate_num_bundles(remainder, 5)
+  num_ones = remainder / 1
+
+  quote = (num_forties * 3.85) + (num_twenties * 1.93) + (num_tens * 0.97) + (num_fives * 0.49) + (num_ones * 0.10)
+  quote.round(2)
+end
+
+def calculate_num_bundles(quantity, bundle_size)
+  return [0, quantity] if quantity < bundle_size
+
+  num_bundles = quantity / bundle_size
+  remainder = quantity % bundle_size
+
+  [num_bundles, remainder]
+end
+# rubocop:enable Metrics/AbcSize
---
+++ cheapest_quote/cheapest_quote_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'cheapest_quote'
+
+describe 'cheapest_quote' do
+  it 'returns cheapest quote based on number of newspapers' do
+    expect(cheapest_quote(1)).to eq 0.10
+    expect(cheapest_quote(5)).to eq 0.49
+    expect(cheapest_quote(10)).to eq 0.97
+    expect(cheapest_quote(20)).to eq 1.93
+    expect(cheapest_quote(40)).to eq 3.85
+    expect(cheapest_quote(41)).to eq 3.95
+    expect(cheapest_quote(80)).to eq 7.70
+    expect(cheapest_quote(26)).to eq 2.52
+    expect(cheapest_quote(0)).to eq 0.0
+    expect(cheapest_quote(499)).to eq 48.06
+  end
+
+  it 'rounds quote to 2 decimal places' do
+    expect(cheapest_quote(21_274)).to eq 2047.65
+  end
+end
