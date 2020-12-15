7c3623dc5eaefecb9144169d4deb37e1ea6c388c
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-12-15 08:38:49 +0000

    Solve kata

---
+++ pillow/pillow.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def pillow(str)
+  fridge_str, pillow_str = str
+
+  fridge_indices = indices_of_char('n', fridge_str)
+  pillow_indices = indices_of_char('B', pillow_str)
+
+  pillow_indices.each_with_index do |pillow_index, index|
+    return true if pillow_index == fridge_indices[index]
+  end
+
+  false
+end
+
+def indices_of_char(char, str)
+  str.enum_for(:scan, /(?=#{char})/).map { Regexp.last_match.offset(0).first }
+end
