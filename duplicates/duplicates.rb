09e4ca3ab58e5915785af4fb1dc93aba73db1e17
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-08-05 13:17:55 +0100

    Simplify solution

--- duplicates/duplicates.rb
+++ duplicates/duplicates.rb
@@ -3,16 +3,5 @@
 def duplicates(arr)
   return [] if arr.nil? || arr.empty?
 
-  traversed = [arr[0]]
-  duplicate_results = []
-
-  arr[1..-1].each do |item|
-    if traversed.include?(item)
-      duplicate_results << item
-    else
-      traversed << item
-    end
-  end
-
-  duplicate_results.uniq
+  arr.select { |v| arr.count(v) > 1 }.uniq
 end
