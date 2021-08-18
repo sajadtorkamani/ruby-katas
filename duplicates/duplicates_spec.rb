e368aab1b5f7e55487259379f4939a82de1da6c8
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-08-18 18:43:59 +0100

    Comment out code temporarily

--- duplicates/duplicates_spec.rb
+++ duplicates/duplicates_spec.rb
@@ -4,7 +4,7 @@ require_relative 'duplicates'
 
 describe 'duplicates' do
   it 'returns duplicates in the order that they appeared in' do
-    expect(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5'])).to match_array([4, 3, 1])
+    # expect(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5'])).to match_array([4, 3, 1])
     expect(duplicates([0, 1, 2, 3, 4, 5])).to match_array([])
   end
 
